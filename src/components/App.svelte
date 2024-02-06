0cae303cc72ff55355a4acb317546c5f8ac9c112
Fran <frangalo34@gmail.com>
2024-02-06 16:59:53 +0100

    Add App schema using svelte.

---
+++ src/components/App.svelte
@@ -0,0 +1,26 @@
+<script>
+          import { Alert } from "flowbite-svelte";
+          import { APP_STATUS, appStatus } from "../store.ts";
+          import StepUploading from "./StepUploading.svelte";
+          import StepLoading from "./StepLoading.svelte";
+          import StepChat from "./StepChat.svelte";
+</script>
+
+{#if $appStatus === APP_STATUS.INIT}
+          <StepUploading />
+{/if}
+{#if $appStatus === APP_STATUS.LOADING}
+          <StepLoading />
+{/if}
+{#if $appStatus === APP_STATUS.ERROR}
+          <Alert>
+                    <span class="font-medium">Algo malo ha pasado</span>
+                    <span class="block sm:inline"
+                              >Por favor, inténtalo de nuevo.</span
+                    >
+          </Alert>
+{/if}
+{#if $appStatus === APP_STATUS.SUCCESS}
+          <p>Ask anything</p>
+          <StepChat />
+{/if}
